a4e937bddbd7fe6b9300d7578414f9325fc3f4f3
stufromnorthvan <stumitchell@irsss.ca>
2022-11-14 18:05:24 -0800

    finish first four questions

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,8 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.first
+@store2 = Store.find_by(id: 2)
+@store1.name = "Ambleside"
+@store1.save
+# puts @store1.name (test)
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create( name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true )
+whistler = Store.create( name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false )
+yaletown = Store.create( name: "Yaletown", annual_revenue: 190000, mens_apparel: true, womens_apparel: true )
+
+@mens_stores = Store.where( mens_apparel: true )
+
+@mens_stores.each do |mens_store|
+  puts "#{mens_store.name} \n #{mens_store.annual_revenue}"
+end
+
+@womens_stores = Store.where( womens_apparel: true )
+
+@womens_stores.each do |womens_store|
+  puts "#{womens_store.name} \n #{womens_store.annual_revenue}"
+end
